c132e742a90ab746098a27e6cacb451e734849a7
hacke <hackerheroXP@outlook.com>
2024-01-06 19:28:30 +0800

    Initial

---
+++ src/main/java/com/maxeu/dynar/DynarClient.java
@@ -0,0 +1,10 @@
+package com.maxeu.dynar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DynarClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
