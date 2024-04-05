255863269bd5ecb16b09740a577d32e2193fe100
3114396pig <2161291955@qq.com>
2024-04-05 19:38:24 +0800

    test

--- src/main/java/com/maxeu/dynar/Dynar.java
+++ src/main/java/com/maxeu/dynar/Dynar.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Dynar implements ModInitializer {
-
-
     public static final String MOD_ID = "dynar";
 	public static final Logger LOGGER = LoggerFactory.getLogger("dynar");
 	@Override
